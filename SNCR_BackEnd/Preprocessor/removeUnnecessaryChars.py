09c2a9ec716ce6ca737714e59caf0a08949145d8
namodaya <namodayabalaarachchi@gmail.com>
2016-07-13 22:21:44 +0530

    remove unnecessary characters

---
+++ SNCR_BackEnd/Preprocessor/removeUnnecessaryChars.py
@@ -0,0 +1,24 @@
+import re
+
+from mysql.connector import (connection)
+
+db = connection.MySQLConnection(user='root', password='1234',
+                                host='127.0.0.1',
+                                database='NewsData',
+                                charset='utf8')
+
+# prepare a cursor object using cursor() method
+cursor = db.cursor()
+
+sql = "SELECT title FROM NewsOrder"
+cursor.execute(sql)
+news = cursor.fetchall()
+
+cyril = re.compile(u"[\u0021-\u007F]+", re.UNICODE)
+# plainText = cyril.sub('',news)
+#
+# print plainText
+
+for row in news:
+    plainText = cyril.sub('', row[0])
+    print plainText
